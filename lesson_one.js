a3a20bd6a6b93843bac08e38a6bb311164da9452
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-02-12 11:33:44 -0500

    fixed some issues, added getter setter and event listener example

--- lesson_one.js
+++ lesson_one.js
@@ -7,7 +7,6 @@
     this.lastName = lastName;
     this.age = age;
     this.howOldWillYouBeIn = function( x ){
-      Radical.prototype.howOldWillIYouBeIn = function( x ){
       console.log( "I'll be", x + this.age,",",x,"years from now!");
     }
   }
@@ -24,6 +23,8 @@
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
+    // its good for maintenance and readability to declare properties, even if they wont have values yet
+    this.view = null;
   }
   // Radical's prototype is an instance of Person... a bit tricky to think about
   // but that's how inheritance happens in JS
@@ -39,22 +40,54 @@
     alert("AAAAAAAAAAAAH!");
   }
 
+  // Mutators
+  // Getters and setters are awesome, always create them in pairs
+  // a good explanation of why
+  // http://stackoverflow.com/questions/1568091/why-use-getters-and-setters
+  // i think of them as the knobs and pits in puzzle pieces.
+  Radical.prototype.getView = function(){
+    return this.element;
+  }
+  Radical.prototype.setView = function( el){
+    /* 
+    in a more complex example we can do stuff here
+    if you just set the property directly, any manipulation
+    would happen outside that class, and then sort of 'be in the wrong place'
+    this would break 'separation of concerns' and 
+    */
+    this.view = el;
+    /*
+    for example here, we can assign the click event listener to our view, that calls our greet method.
+    greet would still have access to all our properties using the "this" keyword.
+    include the value this.view
+    */
+    console.log( el );
+    var me = this;
+    this.view.addEventListener("click", function(){
+      me.greet();
+    }, false );
+  }
+
   // we create a person
   var george = new Person( 'George', 'Constanza', 43, 'male' );
   // and we call a method
   george.greet();
-
   // we create a radical
   var haley = new Radical( "Haley", "Firebrand", 32, 'female' );
+  var drew = new Radical( "Drew", "Hornbein", 26, 'male' );
   // we call the overridden greet method
   haley.greet();
-  // we call an inherited method set up in the 'traditional way'
+  // lets use the setView method to ascribe a view in the dom for haley.
+  console.log( 'document.getElementById("haley"):', document.getElementById("haley") );
+  haley.setView( document.getElementById("haley") );
+  // different "instances" of Radical have different properties based on what was passed to the contructor
+  drew.greet();
+  // we call an inherited method walk which is "encapsulated" into the Radical class because it extends Person.
   haley.walk();
   // we also inherit the 'member method' (the one we defined inside the constructor of the parent class)
-  haley.howOldWillIYouBeIn(5);
+  haley.howOldWillYouBeIn(5);
   // we call our very own method
   haley.yell();
-
   // this keyword usage nside closures
   console.log( this.aThing );
 
